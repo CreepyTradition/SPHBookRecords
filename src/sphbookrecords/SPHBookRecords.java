766dea126146d79d14683c11f4da0f237eeafedb
Perkin Abrantes <perkinabrantes@gmail.com>
2024-03-11 23:10:35 +0800

    Initial commit

---
+++ src/sphbookrecords/SPHBookRecords.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sphbookrecords;
+
+/**
+ *
+ * @author Perks
+ */
+public class SPHBookRecords {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        mainMenu mainmenu = new mainMenu();
+        mainmenu.displayform();
+    }
+    
+}
